1023db9d36a770268d7485fc25fb07b1a3d1165b
YurDan1973 <jura.danilov.73@gmail.com>
2024-12-12 17:54:58 +0300

    Initial commit

--- src/main/java/com/hstn/thymeleaf/controller/PupilController.java
+++ src/main/java/com/hstn/thymeleaf/controller/PupilController.java
@@ -4,6 +4,8 @@ import com.hstn.thymeleaf.model.Pupil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PupilController {
@@ -14,5 +16,11 @@ public class PupilController {
         Pupil pupil = new Pupil();
         model.addAttribute("pupil", pupil);
         return "pupil-form";
+    }
+
+    @PostMapping("/processPupilForm")
+    public String processPupilForm(@ModelAttribute("pupil") Pupil pupil) {
+        System.out.println(pupil);
+        return "pupil-confirmation";
     }
 }
